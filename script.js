c081d7501c21ebc8cd5113eb5f8610a3027f46f0
czhou578 <42430035+czhou578@users.noreply.github.com>
2021-08-09 10:55:10 -0700

    Update script.js
    added new script

---
+++ script.js
@@ -0,0 +1,54 @@
+var movingBtn = document.querySelectorAll('.movingBtn');
+movingBtn[0].style.backgroundColor = "#FFDAB9";
+var listOfPanels = document.querySelectorAll('.achivPanel');
+
+let firstClick = false;
+for (let i = 0; i < movingBtn.length; i++) { //initial click, the first panel gets hidden
+  firstClick = true;
+  movingBtn[i].addEventListener('click', function() {
+    listOfPanels[i].removeAttribute("hidden");
+
+    movingBtn[i].style.backgroundColor = '#FFDAB9';
+    for (let j = 0; j < movingBtn.length; j++) {
+      if (j != i) {
+        listOfPanels[j].setAttribute("hidden", "true");
+        movingBtn[j].style.backgroundColor = 'transparent';
+
+      } 
+    }
+  })
+}
+
+const progressLine = document.querySelector('#connect-line')
+
+window.addEventListener("scroll", function(event) {
+  var top = this.scrollY,
+  left =this.scrollX;
+  if (top >= 533.5 && top <= 800) {
+    progressLine.setAttribute('fill', "url(#right)")
+  }
+});
+
+gsap.registerPlugin(MotionPathPlugin);
+
+MotionPathPlugin.convertToPath('ellipse')
+
+gsap.to('#ellipse', {
+  motionPath: {
+    path: "#path",
+    start: 1,
+    end: 0,
+    align: "#path",
+    alignOrigin: [0.5, 0.5],
+    fill: "4DAF4C"
+  },
+  duration: 7,
+  ease: "power1.inOut"
+})
+
+
+
+
+
+
+
